31ae4665e5d6d465b387350253879bf2f3ccc7bd
Tiago Pinto <tiago.webdev10@gmail.com>
2024-02-05 12:13:55 +0000

    Add Books to the database

--- books/filters.py
+++ books/filters.py
@@ -6,21 +6,31 @@ from .models import Book
 
 
 class BookFilter(django_filters.FilterSet):
-    title_isbn = django_filters.CharFilter(
-        method="fitler_title_isbn",
+    title_isbn_author = django_filters.CharFilter(
+        method="fitler_title_isbn_author",
         label="Search",
         widget=forms.TextInput(
-            attrs={'placeholder': 'Pesquisar por título ou ISBN...'}),
+            attrs={'placeholder': 'Pesquisar por título, autor ou ISBN...'}),
     )
 
     class Meta:
         model = Book
-        fields = ["title_isbn", "author", "publisher", "genre", "language"]
+        fields = ["title_isbn_author",
+                  "genre", "publisher", "language"]
         exclude = ["id", "created", "cover",
-                   "publication_date", "quantity", "summary", "pages", "title", "isbn", "slug"]
+                   "publication_date", "quantity", "summary", "pages", "title", "isbn", "slug", "author"]
 
-    def fitler_title_isbn(self, queryset, name, value):
-        return queryset.filter(
-            Q(title__icontains=value) |
-            Q(isbn__icontains=value)
-        )
+    def fitler_title_isbn_author(self, queryset, name, value):
+        keywords = value.split()
+
+        q_objects = Q()
+
+        for keyword in keywords:
+            q_objects |= (
+                Q(title__icontains=keyword) |
+                Q(isbn__icontains=keyword) |
+                Q(author__first_name__icontains=keyword) |
+                Q(author__last_name__icontains=keyword)
+            )
+
+        return queryset.filter(q_objects).distinct()
