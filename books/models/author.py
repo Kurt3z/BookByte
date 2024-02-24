7ccf45321bb1c3d7d8c71725bca653551a7a27d3
Tiago Pinto <tiago.webdev10@gmail.com>
2024-02-24 00:02:52 +0000

    Commit

--- books/models/author.py
+++ books/models/author.py
@@ -10,12 +10,13 @@ class Author(models.Model):
     created = models.DateTimeField(auto_now=False, auto_now_add=True)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
-    birthdate = models.DateField(auto_now=False, auto_now_add=False)
+    birthdate = models.DateField(
+        auto_now=False, auto_now_add=False, null=True, blank=True)
     country = models.ForeignKey(
         Country, on_delete=models.SET_NULL, null=True, blank=True)
-    biography = models.TextField()
+    biography = models.TextField(null=True, blank=True)
     personal_website = models.URLField(blank=True, null=True)
-    picture = models.ImageField(upload_to="authors/")
+    picture = models.ImageField(upload_to="authors/", null=True, blank=True)
     slug = AutoSlugField(populate_from="get_full_name", unique=True,
                          db_index=True, editable=False, null=True)
 
